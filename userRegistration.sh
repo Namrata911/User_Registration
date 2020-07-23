43cdad10e5684488f4a5f30f67d7027ff37d6d83
Namrata <911npm@gmail.com>
2020-07-23 22:52:27 +0530

    Added code for UC2 to validate last name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,17 +1,27 @@
 #!/bin/bash 
+function validateName(){
+name=$1
 flag=0
 while [ $flag -eq 0 ]
 do
-	echo "Enter a valid user name"
-	echo "A valid user name starts with capital letter and has minimum 3 characters"
-	read firstName
 	pattern="^[A-Z]{1}[a-z]{2}"
 
-	if [[ $firstName =~ $pattern ]]
+	if [[ $name =~ $pattern ]]
 	then
 		echo "Good! It is valid"
 		flag=1
 	else
 		echo "OOps! It is not valid. Enter again... "
+		read name
 	fi
 done
+echo $name
+}
+
+echo "Enter Valid First name (Starts with Capital letter and has min 3 characters)";
+read firstName ;
+validateName $firstName
+
+echo "Enter Valid Last name (starts with capital letter and has min 3 characters)";
+read lastName ;
+validateName $lastName
