6042eadc4ab4789a6fc4c74e41cfa96e645f87b9
Namrata <911npm@gmail.com>
2020-07-23 22:16:27 +0530

    Added code for Use case 1 to validate for First Name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,2 +1,17 @@
-#!/bin/bash
+#!/bin/bash 
+flag=0
+while [ $flag -eq 0 ]
+do
+	echo "Enter a valid user name"
+	echo "A valid user name starts with capital letter and has minimum 3 characters"
+	read firstName
+	pattern="^[A-Z]{1}[a-z]{2}"
 
+	if [[ $firstName =~ $pattern ]]
+	then
+		echo "Good! It is valid"
+		flag=1
+	else
+		echo "OOps! It is not valid. Enter again... "
+	fi
+done
